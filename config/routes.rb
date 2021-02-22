eae5d54b2038dfbf61a54d63f61dd87d09ba66e6
Ryan Woods <contact@ryanofwoods.com>
2021-02-22 16:51:22 +0100

    Add paintings and bookings routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  
+  devise_for :users
+  resources :paintings do
+    resources :bookings, only: [:new]
+  end
+
+  resources :bookings, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
