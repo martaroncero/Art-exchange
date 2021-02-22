696b08ad7e006f2f5ca00119807942791d4f689d
Won Lee <cathykw2018@gmail.com>
2021-02-22 17:29:48 +0000

    added painting model index show new create

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,2 +1,33 @@
 class PaintingsController < ApplicationController
+before_action :set_painting, only: [:show]
+
+def index
+    @paintings = Painting.all
+end
+
+def show
+    @painting = Painting.find(painting_params)
+end
+
+def new
+  @painting = Painting.new
+end
+
+def create
+    @painting = Painting.new(painting_params)
+    if @painting.save
+      redirect_to painting_path(@painting)
+    else
+      render :new
+    end
+end
+
+private
+def painting_params
+  params.require(:painting).permit(:title, :type, :height, :width, :price_cents_per_day, :location, :description)
+end
+end
+
+def set_painting
+  @painting = Painting.find(params[:id])
 end
