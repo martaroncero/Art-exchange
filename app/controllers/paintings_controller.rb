20b1461ec6729fec6da30f872078d723a561a7e0
Won Lee <cathykw2018@gmail.com>
2021-02-22 17:48:04 +0000

    indentation fix

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,33 +1,33 @@
 class PaintingsController < ApplicationController
-before_action :set_painting, only: [:show, :edit, :update, :destroy]
+  before_action :set_painting, only: [:show, :edit, :update, :destroy]
 
-def index
-    @paintings = Painting.all
-end
+  def index
+      @paintings = Painting.all
+  end
 
-def show
-    @painting = Painting.find(painting_params)
-end
+  def show
+      @painting = Painting.find(painting_params)
+  end
 
-def new
-  @painting = Painting.new
-end
+  def new
+    @painting = Painting.new
+  end
 
-def create
-    @painting = Painting.new(painting_params)
-    if @painting.save
-      redirect_to painting_path(@painting)
-    else
-      render :new
-    end
-end
+  def create
+      @painting = Painting.new(painting_params)
+      if @painting.save
+        redirect_to painting_path(@painting)
+      else
+        render :new
+      end
+  end
 
-private
-def painting_params
-  params.require(:painting).permit(:title, :type, :height, :width, :price_cents_per_day, :location, :description)
-end
-end
+  private
+  def painting_params
+    params.require(:painting).permit(:title, :type, :height, :width, :price_cents_per_day, :location, :description)
+  end
 
-def set_painting
-  @painting = Painting.find(params[:id])
+  def set_painting
+    @painting = Painting.find(params[:id])
+  end
 end
