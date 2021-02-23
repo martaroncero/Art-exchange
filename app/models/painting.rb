a174c94d20e5e7537b5a3fe70ddb10b929eeecb6
Ryan Woods <contact@ryanofwoods.com>
2021-02-23 18:22:44 +0100

    Update the catagories validation to be inclusion based off an array and update the form to reflect this

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -4,4 +4,7 @@ class Painting < ApplicationRecord
 
   validates :title, :price_cents_per_day, :width, :height, :category, :location, presence: true
   validates :width, :height, :price_cents_per_day, numericality: { only_integer: true }
+
+  CATEGORIES = ["Acrylic", "Egg Tempera", "Gouache", "Ink", "Oil", "Charcoal", "Graphite", "Chalk Pastel", "Watercolour", "Mixed" ]
+  validates :category, inclusion: { in: CATEGORIES }
 end
