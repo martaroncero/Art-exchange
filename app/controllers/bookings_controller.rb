f77a5cac8e040b9cf059cca188a5104c6dc55fef
gayatriarora <gayatri7arora@gmail.com>
2021-02-23 11:09:07 +0000

    add index and show action to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,16 @@
 class BookingsController < ApplicationController
+  
+
+  def index
+    @user = current_user
+    @bookings = @user.bookings
+    @bookings_through_paintings = @user.paintings_bookings
+  end
+
+  def show
+    @booking = @user.bookings.find(params[:id])
+  end
+  
+
+
 end
