0eaff74d8c5cc5ca13166cdc750d7985ff29ad74
Marta Roncero <m.ronceroolmeda@gmail.com>
2021-02-24 12:38:03 +0000

    Styled form

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -4,10 +4,10 @@ class Painting < ApplicationRecord
   has_one_attached :photo
 
 
-  validates :title, :price_cents_per_day, :width, :height, :location, presence: true
-  validates :width, :height, :price_cents_per_day, numericality: { only_integer: true }
+  # validates :title, :price_cents_per_day, :width, :height, :location, presence: true
+  # validates :width, :height, :price_cents_per_day, numericality: { only_integer: true }
 
   CATEGORIES = ["Acrylic", "Egg Tempera", "Gouache", "Ink", "Oil", "Watercolour", "Mixed"]
 
-  validates :category, inclusion: { in: CATEGORIES, message: "must be from the list" }
+  # validates :category, inclusion: { in: CATEGORIES, message: "must be from the list" }
 end
