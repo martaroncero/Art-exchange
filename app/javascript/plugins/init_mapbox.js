4db51c57473c03992e821c7cc8bc1fb103314a08
Ryan Woods <contact@ryanofwoods.com>
2021-02-25 12:48:32 +0100

    Add js to init_mapbox to translate dataset to markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,6 +10,13 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .addTo(map);
+    });
   }
 };
 
