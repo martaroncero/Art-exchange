877af7d76014c510fded58722a9c0b4b89ef30e0
Ryan Woods <contact@ryanofwoods.com>
2021-02-25 12:24:09 +0100

    db migrate ran and geocode add to paintings model based on location

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -3,11 +3,12 @@ class Painting < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
 
+  CATEGORIES = ["Acrylic", "Egg Tempera", "Gouache", "Ink", "Oil", "Watercolour", "Mixed"]
 
   validates :title, :price_cents_per_day, :width, :height, :location, presence: true
   validates :width, :height, :price_cents_per_day, numericality: { only_integer: true }
-
-  CATEGORIES = ["Acrylic", "Egg Tempera", "Gouache", "Ink", "Oil", "Watercolour", "Mixed"]
-
   validates :category, inclusion: { in: CATEGORIES, message: "must be from the list" }
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
