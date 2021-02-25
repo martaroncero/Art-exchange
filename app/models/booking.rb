9d243604fe925f583cff228cade0f652223b82be
Ryan Woods <contact@ryanofwoods.com>
2021-02-25 19:47:06 +0100

    add js form reload

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,11 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :start_date, :end_date, presence: true
+  validate :start_not_after_end_date
+
+  def start_not_after_end_date
+    if end_date.present? && start_date > end_date
+      errors.add(:start_date, "cannot be after the end date")
+    end
+  end
 end
