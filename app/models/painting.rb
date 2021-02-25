198388cc206f8ee4537dc28e0d545e628678ea95
Won Lee <cathykw2018@gmail.com>
2021-02-25 15:16:01 +0000

    fix

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -5,7 +5,7 @@ class Painting < ApplicationRecord
 
 include PgSearch::Model
   pg_search_scope :search_by_title_and_description_and_category,
-    against: [ :title, :description, :category, :location ],
+    against: [ :title, :description, :category ],
     using: {
       tsearch: { prefix: true }
     }
