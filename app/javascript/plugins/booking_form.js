3b30257278f45163fd4ccc2f01ebcd3d42610811
Ryan Woods <contact@ryanofwoods.com>
2021-02-26 12:53:01 +0100

    fix booking form update logic

--- app/javascript/plugins/booking_form.js
+++ app/javascript/plugins/booking_form.js
@@ -22,7 +22,8 @@ const updateCost = () => {
         endDateElem.classList.add("is-invalid");
       } else {
         let diff = end - start;
-        if (diff < 0) {
+        let days = Math.ceil(diff / (1000 * 3600 * 24)) + 1;
+        if (days <= 0) {
           costElement.innerText = "The end date cannot be before your start date";
           endDateElem.classList.remove("is-valid");
           endDateElem.classList.add("is-invalid");
@@ -30,10 +31,9 @@ const updateCost = () => {
           startDateElem.classList.add("is-invalid");
         } else {
           startDateElem.classList.remove("is-invalid");
-          endDateElem.classList.remove("is-invalid");
           startDateElem.classList.add("is-valid");
+          endDateElem.classList.remove("is-invalid");
           endDateElem.classList.add("is-valid");
-          let days = Math.ceil(diff / (1000 * 3600 * 24)) + 1;
           costElement.innerText = `Total cost: ${days}`;
         }  
       }
