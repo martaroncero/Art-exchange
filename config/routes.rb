2b77789e4e4e04d9d478dc1f64af0c7f29daf49e
Ryan Woods <contact@ryanofwoods.com>
2021-04-29 09:16:15 +0200

    remove default comment on routes and format code

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
   devise_for :users
+
   resources :paintings do
     resources :bookings, only: [:new, :create]
   end
 
   resources :bookings, only: [:index, :show, :destroy]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
